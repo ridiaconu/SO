2e541e11b6de85ed0a64b2b5fc84cdf0b23f524c
Radu Diaconu <77971744+ridiaconu@users.noreply.github.com>
2023-01-05 00:22:49 +0200

    Update PIPES.cpp

--- PIPES.cpp
+++ PIPES.cpp
@@ -2,28 +2,39 @@
 
 using namespace std;
 
-int main() {
-   for (int i = 1; i <= 10000; i++) {
-      bool is_prime = true;
+int main()
 
-      if (i < 2) {
-         is_prime = false;
-      }
-      else if (i % 2 == 0 && i > 2) {
-         is_prime = false;
-      }
-      else {
-         for (int d = 3; d * d <= i && is_prime; d++) {
-            if (i % d == 0) { 
-               is_prime = false;  
-            } 
-         }
-      }
+{
 
-      if (is_prime) {
-         cout << i << " ";
-      }
+   int i;
+
+   for(i=1;i<=10000;i++)
+
+   {
+
+       int is_prime = 1;
+
+       if(i<2)
+
+	{
+           is_prime = 0;
+	}
+
+       if(i % 2 == 0 && i > 2)
+
+	{
+           is_prime = 0;
+	}
+
+       for(int d = 3; d * d <= i && is_prime == 1; d++)
+
+           if(i % d == 0) 
+	    { 
+               is_prime = 0;  
+	    } 
+
+       if(is_prime == 1)
+
+           cout<<i<<" ";
    }
-
-   return 0;
 }
